0473bbf019feb2a748dd57ad1bcf615d477effb8
Austin Riendeau <austin@apriendeau.com>
2023-01-26 20:51:29 -0700

    switch to socketmode

--- reservebot.go
+++ reservebot.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"net/http"
+	"os"
 	"time"
 
 	"github.com/ameliagapin/reservebot/data"
@@ -14,11 +12,13 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
 var (
 	token          string
 	challenge      string
+	appToken       string
 	listenPort     int
 	debug          bool
 	admins         string
@@ -31,6 +31,7 @@ var (
 func main() {
 	flag.StringVar(&token, "token", util.LookupEnvOrString("SLACK_TOKEN", ""), "Slack API Token")
 	flag.StringVar(&challenge, "challenge", util.LookupEnvOrString("SLACK_CHALLENGE", ""), "Slack verification token")
+	flag.StringVar(&appToken, "appToken", util.LookupEnvOrString("SLACK_APP_TOKEN", ""), "Slack App token")
 	flag.IntVar(&listenPort, "listen-port", util.LookupEnvOrInt("LISTEN_PORT", 666), "Listen port")
 	flag.BoolVar(&debug, "debug", util.LookupEnvOrBool("DEBUG", false), "Debug mode")
 	flag.StringVar(&admins, "admins", util.LookupEnvOrString("SLACK_ADMINS", ""), "Turn on administrative commands for specific admins, comma separated list")
@@ -49,8 +50,12 @@ func main() {
 		log.Error("Slack verification token is required")
 		return
 	}
-
-	api := slack.New(token, slack.OptionDebug(debug))
+	log.Info(token, appToken)
+	api := slack.New(
+		token,
+		slack.OptionDebug(debug),
+		slack.OptionAppLevelToken(appToken),
+	)
 
 	data := data.NewMemory()
 
@@ -68,58 +73,45 @@ func main() {
 				}
 			}
 		}()
-
 	} else {
 		log.Infof("Automatic pruning is disabled.")
 	}
 
 	handler := handler.New(api, data, reqResourceEnv, util.ParseAdmins(admins))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(""))
-	})
+	client := socketmode.New(
+		api,
+		socketmode.OptionDebug(true),
+	)
 
-	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-		body := buf.String()
+	go func() {
+		for evt := range client.Events {
+			switch evt.Type {
+			case socketmode.EventTypeConnecting:
+				log.Info("Connecting to Slack with Socket Mode...")
+			case socketmode.EventTypeConnectionError:
+				log.Info("Connection failed. Retrying later...")
+			case socketmode.EventTypeConnected:
+				log.Info("Connected to Slack with Socket Mode.")
+			case socketmode.EventTypeEventsAPI:
+				eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					fmt.Printf("Ignored %+v\n", evt)
+					continue
+				}
 
-		api.Debugf("Request: %s", body)
+				fmt.Printf("Event received: %+v\n", eventsAPIEvent)
+				client.Ack(*evt.Request)
 
-		eventsAPIEvent, err := slackevents.ParseEvent(
-			json.RawMessage(body),
-			slackevents.OptionVerifyToken(
-				&slackevents.TokenComparator{VerificationToken: challenge},
-			),
-		)
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Errorf("%+v", err)
-			return
-		}
-
-		switch eventsAPIEvent.Type {
-		case slackevents.URLVerification:
-			var r *slackevents.ChallengeResponse
-			err := json.Unmarshal([]byte(body), &r)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				if err := handler.CallbackEvent(eventsAPIEvent); err != nil {
+					log.Errorf("%+v", err)
+				}
+			default:
+				fmt.Fprintf(os.Stderr, "Unexpected event type received: %s\n", evt.Type)
 			}
-			w.Header().Set("Content-Type", "text")
-			w.Write([]byte(r.Challenge))
-		case slackevents.CallbackEvent:
-			err := handler.CallbackEvent(eventsAPIEvent)
-			if err != nil {
-				log.Errorf("%+v", err)
-			}
-		default:
 		}
-	})
-
-	log.Infof("Server listening on port %d", listenPort)
-
-	http.ListenAndServe(fmt.Sprintf(":%v", listenPort), nil)
+	}()
+	log.Infof("Starting Event Socket", listenPort)
+	client.Run()
 
 }
